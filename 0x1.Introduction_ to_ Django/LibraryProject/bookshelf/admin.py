cd40313ba6d59d2072de28a54d57e01c803cea3f
joram kamau <yllejorum@gmail.com>
2024-08-13 05:39:37 +0300

    list filter configuration

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Book
+from .models import Books
+
+"list_filter", "author", "publication_year"
 
 # Register your models here.
 "register," 'admin.ModelAdmin'

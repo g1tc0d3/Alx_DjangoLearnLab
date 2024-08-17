e76ef2766161d2b926c6ecaa8535883092fd876d
joram kamau <yllejorum@gmail.com>
2024-08-17 22:59:16 +0300

    self.name

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Author (models.Model):
-    self.name = models.CharField()
+    #name = models.CharField()
+    return self.name
     
     
 

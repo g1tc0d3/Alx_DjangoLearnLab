b6ba41421fa41fa7d6e2f66b12ffe6d37de1dacb
joram kamau <yllejorum@gmail.com>
2024-08-17 23:30:42 +0300

    made corrections to query file for objects

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -6,4 +6,5 @@ Library.objects.get(name=library_name)
 books.all()
 
 Author.objects.get(name=author_name)
-objects.filter(author=author )
+objects.filter(author=author)
+#objects.filter(author=author )

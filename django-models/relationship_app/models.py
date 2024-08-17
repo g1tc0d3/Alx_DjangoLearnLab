e97cf93b9a588059d22959d318beaf5d5c311a8d
joram kamau <yllejorum@gmail.com>
2024-08-17 08:07:30 +0300

    modified class function to include models. Model

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Author ():
+class Author (models.Model):
     name = models.CharField()
 
-class Book ():
+class Book (models.Model):
     title = models.CharField()
     author = models.ForeignKey(Author)
-    
-class Library ():
+
+class Library (models.Model):
     name = models.CharField()
     books = models.ManyToManyField(Book)
 
-class Librarian ():
+class Librarian (models.Model):
     name = models.CharField
     library = models.ManyToManyField(Library)

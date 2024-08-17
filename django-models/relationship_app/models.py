e64194cf19b526d92b627988bd33e334e2cb8151
joram kamau <yllejorum@gmail.com>
2024-08-17 23:03:00 +0300

    done away with white spaces

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -12,10 +12,10 @@ class Book(models.Model):
     title = models.CharField()
     author = models.ForeignKey(Author)
 
-class Library (models.Model):
+class Library(models.Model):
     name = models.CharField()
     books = models.ManyToManyField(Book)
 
-class Librarian (models.Model):
+class Librarian(models.Model):
     name = models.CharField
     library = models.ManyToManyField(Library)

5de8e427b31300fd47669ccb045d352ceb186f98
joram kamau <yllejorum@gmail.com>
2024-08-18 07:07:19 +0300

    first commit for views file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from LibraryProject import LibraryProject
+from relationship_app import Book 
 
 # Create your views here.
+relationship_app/list_books.html
+Book.object.all()
+
+

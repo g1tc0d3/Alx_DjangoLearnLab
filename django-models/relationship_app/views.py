532071c531a041aee680352891cfbc0f556e6104
joram kamau <yllejorum@gmail.com>
2024-08-18 07:12:29 +0300

    added library detail

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from LibraryProject import LibraryProject
-from relationship_app import Book 
+from .models import Library
 
 # Create your views here.
 relationship_app/list_books.html
 Book.objects.all()
+
+relationship_app/library_detail.html
+Library
 
 

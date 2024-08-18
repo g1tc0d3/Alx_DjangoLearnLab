48e7522ae09f67808a8c1001de7b0996b283aaf3
joram kamau <yllejorum@gmail.com>
2024-08-18 08:17:44 +0300

    added User profile to models.py file

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class UserProfile(models.Model):
+    Admin, Member
+
+    
 class Author(models.Model):
     #name = models.CharField()
     return self.name

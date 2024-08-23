4e53f8fc46975b7d84a6235ccf033d11942d006f
joram kamau <yllejorum@gmail.com>
2024-08-23 23:08:04 +0300

    corrected abstract user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
@@ -9,8 +9,7 @@ class Books ():
     publication_year = models.IntegerField ()
     
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField
     profile_photo = models.ImageField
 
-    

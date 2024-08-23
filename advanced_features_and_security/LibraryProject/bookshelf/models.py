df9de8344d1196d8cbb52df137e1187f2b4e0514
joram kamau <yllejorum@gmail.com>
2024-08-23 23:04:48 +0300

    modified models and settings file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
 class Books ():
     title = models.CharField (max_length= 200)
     author = models.CharField (max_length= 100)
     publication_year = models.IntegerField ()
+    
+
+class CustomUser(AbstractBaseUser):
+    date_of_birth = models.DateField
+    profile_photo = models.ImageField
+
     

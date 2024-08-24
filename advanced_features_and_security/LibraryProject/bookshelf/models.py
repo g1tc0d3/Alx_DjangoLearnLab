1bcda28c9b3236b247e0251c89d30050c2afc91e
joram kamau <yllejorum@gmail.com>
2024-08-25 01:44:31 +0300

    modification done to custom permission

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField (max_length= 200)
     author = models.CharField (max_length= 100)
     publication_year = models.IntegerField ()

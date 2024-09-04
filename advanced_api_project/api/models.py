3eeea5863ad6eacefff1a1d8c33529c8e72cc0a6
joram kamau <yllejorum@gmail.com>
2024-09-04 09:54:16 +0300

    setting up django rest framework with custom serializers

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=250,)
+    pass
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    publication_year = models.IntegerField
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    pass
+
---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,6 @@
+# from django.urls import path, include
+# from models import Book, Author
+# from rest_framework.routers import DefaultRouter
+
+# urlpatterns =[
+#     path ('', include (router.urls)),

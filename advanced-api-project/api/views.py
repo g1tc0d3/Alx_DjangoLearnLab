375720a0a33cabf14ef08cb62df0b8eafcc716f9
joram kamau <yllejorum@gmail.com>
2024-09-06 23:32:22 +0300

    implementation of search filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics
+from django_filters import rest_framework
+from rest_framework import generics, filters
 from .models import Book, Author
 from .serializers import AuthorSerializer, BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 
 
 class CustomBookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    filter_backends = [filters.SearchFilter]
+    serach_fields = ['title', 'author']
 
 class CustomBookDetailView(generics.DetailAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
 class CustomBookCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
 class CustomBookUpdateView(generics.UpdateAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
 class CustomBookDeleteView(generics.DeleteAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    queryset = Book.objects.all()

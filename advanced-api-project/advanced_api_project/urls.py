99354dd2775afd978207dc256b95522b5d6f6393
joram kamau <yllejorum@gmail.com>
2024-09-06 22:39:27 +0300

    api urls added to project

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from . import 
 
 router = DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Book/', router.urls),
+    path('api', include api.urls),
 ]

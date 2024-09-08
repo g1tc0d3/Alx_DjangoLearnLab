c55b0bfaf9f377a34b6eb15006d6a1f3c6830437
joram kamau <yllejorum@gmail.com>
2024-09-08 04:06:54 +0300

    unit testing models

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 from api.models import Book
 
+class BookTestCase(TestCase):
+    def setup (self):
+        Book.objects.create()
+        Book.objects.order()
+        Book.objects.filter()
+        Book.objects.search()
+        

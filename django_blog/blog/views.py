3b3a417f77b9a7a295748eeecbcae27182f05ab4
joram kamau <yllejorum@gmail.com>
2024-09-17 11:01:43 +0300

    authentication files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+
+class PostListCreateApiView(generics.ListCreateAPIView):
+    authentication_classes = TokenAuthentication
+    permission_classes = IsAuthenticated
+    queryset = Post.Objects.all()
+    serializer_class = PostSerializer
+
+

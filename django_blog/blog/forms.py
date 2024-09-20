c6c67862dcf970c1965273df08e25386934e38fc
joram kamau <yllejorum@gmail.com>
2024-09-20 09:40:20 +0300

    updated forms.py file

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from models import CustomUser, Post
+from models import CustomUser, Post, Comment
 
-class CommentForm(UserCreationForm):
-    pass 
+class CommentForm(forms.ModelForm):
+    model = Comment
+    queryset = 'content'
+    

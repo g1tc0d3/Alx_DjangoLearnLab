391ec8a39ef9bd23729afe02c48491a78cbf0fd6
joram kamau <yllejorum@gmail.com>
2024-09-25 19:31:30 +0300

    social media api file added

---
+++ social_media_api/social_media_api/accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(max_length=250)
+    profile_picture = models.ImageField
+    followers = models.ManyToManyField(symmetrical=False)
